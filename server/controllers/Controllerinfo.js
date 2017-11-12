f915c9865a5213e80fa700dffee9df8ebaecb536
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-11-12 17:32:29 +0530

    food database

--- server/controllers/Controllerinfo.js
+++ server/controllers/Controllerinfo.js
@@ -3,7 +3,7 @@ var url        = require('url');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
-  password : 'kshaikh',
+  password : 'honor4c',
   database : 'hotel'
 });
 
